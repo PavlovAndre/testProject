4ad8d6fa9d79e0990ebfaa3bf353ee27a23b8a3d
Your Name <you@example.com>
2022-02-04 21:54:40 +0500

    add count of words

--- repitTask/views.py
+++ repitTask/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home (request):
     return render(request, 'home.html')
+def reverse (request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+
+    word_list = user_text.split()
+    count_word = len(word_list)
+    return render(request, 'reverse.html', {'usertext': user_text,
+                                            'reversedtext': reversed_text, 'countword': count_word})
